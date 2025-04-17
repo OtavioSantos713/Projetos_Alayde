db7c545bc789558b49456f4662dfb7d6724d5873
OtavioSantos713 <otavioasn2008@gmail.com>
2025-04-17 15:00:00 -0300

    Add files via upload

---
+++ variavel.py
@@ -0,0 +1,31 @@
+peso=float()
+altura=float()
+nome=str()
+idade=int()
+
+print(peso)
+print(nome)
+print(idade)
+print(altura)
+
+print(type (nome))#inpime o tipo da variável
+print(type (altura))
+print(type (idade))
+print(type (peso))
+
+nome=str(input("Digitar Seu Nome:"))
+peso=float(input("Digitar Seu Peso:"))
+altura=float(input("Digitar Seu Altura:"))
+idade=int(input("Digitar Seu Idade:"))
+
+print("O Tipo Da Variável Nome é =", str)
+print("O Tipo Da Variável Altura é =", float)
+print("O Tipo Da Variável Idade é =", int)
+print("O Tipo Da Variável Peso é =", float)
+
+print("Seu Peso é =", peso)
+print("Sua Altura é =", altura)
+print("Seu Nome é =", nome)
+print("Sua Idade é=", idade)
+
+input#pede par o usuario digitar
